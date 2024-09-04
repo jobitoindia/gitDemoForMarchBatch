5ba910726b767fae504b2789327366c2d935a618
lahupande01 <lahupande01@gmail.com>
2024-09-04 07:45:10 +0530

    i have add code for registration

--- src/gitDemoForMarchBatch/registration.java
+++ src/gitDemoForMarchBatch/registration.java
@@ -1,5 +1,9 @@
 package gitDemoForMarchBatch;
 
-public class registration {
+public class registration 
+{
+	public static void main(String[] args) {
+		System.out.println("i have add the sysout");
+	}
 
 }
